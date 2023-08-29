270b3de2d2a90c6ab0c0e5f3f28f198085638822
Vinamra Mishra <vinamra11@gmail.com>
2023-08-29 13:26:08 +0530

    Frontend Connected for Query

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'chrome-extension://coohjcphdfgbiolnekdpbcijmhambjff' //for postman tab extension
+    'chrome-extension://coohjcphdfgbiolnekdpbcijmhambjff', //for postman tab extension
+    'http://localhost:5173' //react-client
 ];
 
 export default allowedOrigins;
